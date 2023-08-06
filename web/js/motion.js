cc898ddd8afc39d69555cfe6a657fe2999d723ca
BlueberryOreo <1761988645@qq.com>
2023-08-06 23:18:50 +0800

    problem solved

--- web/js/motion.js
+++ web/js/motion.js
@@ -6,6 +6,9 @@ const model = document.getElementById("model");
 
 // tap
 // 问题：drag由于存在鼠标点击的动作也会被判断成click从而触发戳一戳
+// 20230806_23_10 上述问题已解决，方法：添加变量mouseMoved，判断鼠标是否移动过
+//      如果移动过，则设为true，并在drag结束后1ms以后将其恢复为false
+//      根据猜测，click会在鼠标抬起时触发，在鼠标抬起后经过一点延迟再将mouseMove恢复为false可以做到触发click但是不会执行内部的if语句的效果。
 container.addEventListener("click", (event) => {
     // alert(model.autoplay);
     if(!model.paused && !isTapping && !mouseMoved){
@@ -32,12 +35,16 @@ container.addEventListener("mousedown", (event) => {
 
 container.addEventListener("mouseup", (event) => {
     isMouseDown = false;
-    mouseMoved = false;
+    setTimeout(() => {
+        mouseMoved = false;
+    }, 1); // 添加延时1ms
     // window.PyHandler.dragging("mouse up");
 });
 
 container.addEventListener("mousemove", (event) => {
     if(isMouseDown){
+        // 改进想法：更合理的判断鼠标是否移动，因为如果鼠标在点击的时候出现了微小偏移，那么根据这种方法会被判断成拖动
+        // 建议设为判断鼠标移动的时间或者根据一次移动的位置是否在一定阈值内判断是否为点击操作
         mouseMoved = true;
         const mouseX = event.screenX;
         const mouseY = event.screenY;
