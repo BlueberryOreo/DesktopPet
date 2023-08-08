daa63f7eec605ccb0f8f0b3fa32384010ef9c02b
BlueberryOreo <1761988645@qq.com>
2023-08-08 16:10:41 +0800

    add hide console

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from window import *
 from PyQt5.QtWidgets import QApplication
 import sys, os
+import ctypes
 
 from utils import config
 
@@ -14,4 +15,8 @@ def start():
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
+    whnd = ctypes.windll.kernel32.GetConsoleWindow()
+    if whnd != 0:
+        ctypes.windll.user32.ShowWindow(whnd, 0)
+        ctypes.windll.kernel32.CloseHandle(whnd)
     start()
