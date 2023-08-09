9126166326810e7d8018fb3f1f352b1b314b0636
BlueberryOreo <1761988645@qq.com>
2023-08-09 14:30:59 +0800

    update welcome page

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 from window import *
 from PyQt5.QtWidgets import QApplication
-import sys, os
+from threading import Thread
+import sys
 import ctypes
+from time import sleep
 
-from utils import config
+from utils import config, StartWindow
 
 def start():
     app = QApplication(sys.argv)
-    # default_pose_path = "." + config['model']['path'] + "/" + config['model']['default-pose-name']
-    # print(g_construct_path(config['model']['path'], config['model']['default-pose-name']))
-    # print(default_pose_path)
+
+    start_win = StartWindow()
+    start_win.show()
+
     pet = DesktopPet()
     pet.show()
+    
+    sleep(1)
+    start_win.close()
     sys.exit(app.exec_())
 
-if __name__ == "__main__":
+def hide_command():
     whnd = ctypes.windll.kernel32.GetConsoleWindow()
     if whnd != 0:
         ctypes.windll.user32.ShowWindow(whnd, 0)
         ctypes.windll.kernel32.CloseHandle(whnd)
+
+if __name__ == "__main__":
+    hide_command_thread = Thread(target=hide_command)
+    hide_command_thread.start()
     start()
+    hide_command_thread.join()
